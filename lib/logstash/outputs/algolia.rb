7930d7c8135f3369c6356e48d8a60a33e028b6d5
Florent Baldino <florent.baldino@wizbii.com>
2018-10-04 10:30:42 +0200

    Add an 'action' config to allow deletions

--- lib/logstash/outputs/algolia.rb
+++ lib/logstash/outputs/algolia.rb
@@ -5,9 +5,20 @@ require "algoliasearch"
 # An algolia output that does nothing.
 class LogStash::Outputs::Algolia < LogStash::Outputs::Base
   config_name "algolia"
-  
+
+  VALID_ACTIONS = ["delete", "index"]
+
   config :application_id, :validate => :string, :required => true
   config :api_key, :validate => :string, :required => true
+  config :index, :validate => :string, :required => true
+
+  # The Algolia action to perform. Valid actions are:
+  #
+  # - index: indexes a document (an event from Logstash).
+  # - delete: deletes a document by id (An 'objectID' field is required for this action)
+  # - A sprintf style string to change the action based on the content of the event. The value `%{[foo]}`
+  #   would use the foo field for the action
+  config :action, :validate => :string, :required => false, :default => "index"
 
   def register
     @algolia_client = Algolia.init(application_id: @application_id, api_key: @api_key)
@@ -17,15 +28,22 @@ class LogStash::Outputs::Algolia < LogStash::Outputs::Base
 
   def multi_receive(events)
     events
-      .group_by { |e| e.get('_index') }
-      .each do |index_name, events|
-        # todo: group `add_objects` calls by bulks of 10MB max
-        partitions(events).each do |events_group|
-          get_index(index_name).add_objects(events_group.map(&:to_hash))
-        end
+      .group_by { |e| e.sprintf(@action) }
+      .select { |action, events| valid_action?(action) }
+      .each do |action, events|
+        events
+          .select { |e| valid_event?(e, action) }
+          .group_by { |e| e.sprintf(@index) }
+          .each do |index_name, events|
+            partitions(events).each do |events_group|
+              do_action(action, events, get_index(index_name))
+            end
+          end
       end
+
     nil
-  end # def event
+  end # def multi_receive
+
 
   def get_index(name)
     @indices[name] ||= Algolia::Index.new(name)
@@ -33,5 +51,32 @@ class LogStash::Outputs::Algolia < LogStash::Outputs::Base
 
   def partitions(events)
     [events]
+  end
+
+  def valid_action?(action)
+    return true if VALID_ACTIONS.include?(action)
+
+    @logger.error("Invalid action given: '#{action}'")
+
+    false
+  end
+
+  def valid_event?(e, action)
+    if action == "delete" && e.get("objectID").nil?
+      @logger.error("Invalid event for 'delete' action: no 'objectID' field")
+
+      return false
+    end
+
+    true
+  end
+
+  def do_action(action, events, algolia_index)
+    case action
+    when "index"
+      algolia_index.add_objects(events_group.map(&:to_hash))
+    when "delete"
+      algolia_index.delete_objects(events_group.map { |e| e.get("objectID") })
+    end
   end
 end # class LogStash::Outputs::Algolia
